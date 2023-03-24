14acdc5d81ac888232b4734d0d047f52bde507cb
Elvin Zhang <elvinzhang0@gmail.com>
2023-03-24 18:01:11 -0400

    Init project.

---
+++ EmployeeDir/EmployeeDirApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeDirApp.swift
+//  EmployeeDir
+//
+//  Created by Elvin Zhang on 2023-03-24.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeDirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
